f2e859b8f2a168e6e4783034ff86629776e6478e
MikebriFX <mikeb@fnf.co.za>
2020-11-26 10:28:16 +0200

    Created entities and BookStoreDbContext

---
+++ src/BookStore.Infrastructure/Context/BookStoreDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Infrastructure.Context
+{
+    public class BookStoreDbContext : DbContext
+    {       
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options) { }
+
+        public BookStoreDbContext() { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(150)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookStoreDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
